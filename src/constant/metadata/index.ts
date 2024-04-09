0741997d854208a617711c0c016524cc2a2beb0d
Muhammad Rifqi Adli Gumay <rifqiadli@Muhammads-MacBook-Pro.local>
2024-04-09 19:32:50 +0700

    change metadata

--- src/constant/metadata/index.ts
+++ src/constant/metadata/index.ts
@@ -1,6 +1,6 @@
 const METADATA = {
   creator: 'Muhammad Rifqi Adli Gumay',
-  description: 'Personal website, website portofolio, portofolio, rifqi adli blog, znovadap front-end web developer',
+  description: 'Personal website, website portofolio, portofolio, rifqi adli blog, rifqiadli full-stack web developer',
   keyword: 'rifqi adli, rifqi adli personal website, rifqi adli portofolio, rifqi adli blog, Muhammad Rifqi Adli Gumay, front-end web developer, front-end engineer',
   authors: {
     name: 'Muhammad Rifqi Adli Gumay',
